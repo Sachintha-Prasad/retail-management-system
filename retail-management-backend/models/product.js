bf98b33a30a0442022bf6c83dd95b62abc1cfb51
Sachintha-Prasad <prasadsachintha1231@gmail.com>
2025-05-13 18:46:57 +0530

    update

--- retail-management-backend/models/product.js
+++ retail-management-backend/models/product.js
@@ -10,7 +10,16 @@ const productSchema = new mongoose.Schema(
         stock: { type: Number, required: true },
         featured: { type: Boolean, default: false }
     },
-    { timestamps: true }
+    {
+        timestamps: true,
+        toJSON: {
+            transform(doc, ret) {
+                ret.id = ret._id
+                delete ret._id
+                delete ret.__v
+            }
+        }
+    }
 )
 
 export const Product = mongoose.model("Product", productSchema)
